4454645fca8eeedd09c32cbeb5bc7e7a224d1d77
YvngGreatness1738 <135516356+EzraMachoka@users.noreply.github.com>
2023-10-04 02:53:16 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -26,15 +26,54 @@ class Signup(Resource):
         db.session.add(user)
         db.session.commit()
         return user.to_dict(), 201
+    def post(self):
+        json = request.get_json()
+        username = json.get('username')
+        password = json.get('password')
+        
+        if not username or not password:
+            return {'message': 'Both username and password are required'}, 400
+
+        
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return {'message': 'Username already exists'}, 400
+
+        
+        user = User(username=username)
+        user.password_hash = password  
+        db.session.add(user)
+        db.session.commit()
+
+        
+        session['user_id'] = user.id
+
+        return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 204  
 
 class Login(Resource):
-    pass
+    def post(self):
+        json_data = request.get_json()
+        print(f'json_data: {json_data}')  
+        username = json_data.get('username')
+        print(f'username: {username}')    
+        password = json_data.get('password')
+
+        
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {'message': '204: No Content'}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
